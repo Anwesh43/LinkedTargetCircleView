d335e063fe4d40f8fc694f8412a45df98cb3e30f
Anwesh43 <anweshthecool0@gmail.com>
2020-10-12 04:23:38 +0530

    adding TargetCircleLineView class

--- targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
+++ targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
@@ -66,4 +66,19 @@ fun Canvas.drawTCLNode(i : Int, scale : Float, paint : Paint) {
     drawTargetCircleLine(scale, w, h, paint)
 }
 
+class TargetCircleLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
 
