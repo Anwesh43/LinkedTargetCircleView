2307f4e52878e6978d7ec11ba792bf9695baf7bb
Anwesh43 <anweshthecool0@gmail.com>
2020-10-12 04:47:34 +0530

    adding TargetCircleLine class

--- targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
+++ targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
@@ -170,5 +170,28 @@ class TargetCircleLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TargetCircleLine(var i : Int) {
+
+        private var curr : TCLNode = TCLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
 
