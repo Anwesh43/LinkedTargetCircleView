0331a74a1ea8db864af53b1339e56ebab4d973fb
Anwesh43 <anweshthecool0@gmail.com>
2020-10-12 03:50:09 +0530

    adding Int and Float extension methods

--- targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
+++ targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
@@ -28,3 +28,8 @@ val sizeFactor : Float = 3.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i / n)
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
