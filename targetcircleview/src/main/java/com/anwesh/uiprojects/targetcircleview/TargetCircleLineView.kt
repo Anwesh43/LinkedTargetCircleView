0fcf69d1ac7f9dd969e9aaa73d1506311851a21d
Anwesh43 <anweshthecool0@gmail.com>
2020-10-12 04:44:31 +0530

    adding TCLNode class

--- targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
+++ targetcircleview/src/main/java/com/anwesh/uiprojects/targetcircleview/TargetCircleLineView.kt
@@ -129,5 +129,46 @@ class TargetCircleLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TCLNode(var i : Int, val state : State = State()) {
+
+        private var next : TCLNode? = null
+        private var prev : TCLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TCLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTCLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TCLNode {
+            var curr : TCLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
