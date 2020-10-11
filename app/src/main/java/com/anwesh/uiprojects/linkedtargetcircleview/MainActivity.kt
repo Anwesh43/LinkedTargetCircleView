0c41526fcaba226349c7e1fcc14ad0c273f9d12f
Anwesh43 <anweshthecool0@gmail.com>
2020-10-12 05:03:26 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtargetcircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtargetcircleview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedtargetcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.targetcircleview.TargetCircleLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TargetCircleLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
